09702fc0d3ebcbd43cf8605b9f0af3d58b839c18
DOWON LEE <domo7304@naver.com>
2020-11-27 22:03:27 +0900

    Chap14.File Processing

---
+++ Chap14. File Processing - lecture/Chap14. File Processing - lecture/Fig.14.11.cpp
@@ -0,0 +1,41 @@
+//#include <iostream>
+//#include <fstream>
+//#include <cstdlib>
+//#include "ClientData.h"
+//using namespace std;
+//
+//int main() {
+//	fstream outCredit("credit.dat", ios::in | ios::out | ios::binary);
+//
+//	if (!outCredit) {
+//		cerr << "File could not be opened." << endl;
+//		exit(EXIT_FAILURE);
+//	}
+//
+//	cout << "Enter account number (1 to 100, 0 to end input)\n? ";
+//
+//	int accountNumber;
+//	string lastName;
+//	string firstName;
+//	double balance;
+//
+//	cin >> accountNumber;
+//
+//	while (accountNumber > 0 && accountNumber <= 100) {
+//		cout << "Enter lastname, firstname and balance\n? ";
+//		cin >> lastName;
+//		cin >> firstName;
+//		cin >> balance;
+//
+//		ClientData client{ accountNumber, lastName, firstName, balance };
+//
+//		outCredit.seekp(
+//			(client.getAccountNumber() - 1) * sizeof(ClientData));
+//
+//		outCredit.write(
+//			reinterpret_cast<const char*>(&client), sizeof(ClientData));
+//
+//		cout << "Enter acocunt number\n? ";
+//		cin >> accountNumber;
+//	}
+//}
